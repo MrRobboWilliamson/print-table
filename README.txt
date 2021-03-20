ecf72fd7ec171b87ad6c791824f9c661ab85b63f
MrRobboWilliamson <robmwilliamson@gmail.com>
2021-03-20 11:27:16 +1000

    setup package structure

---
+++ README.txt
@@ -0,0 +1,13 @@
+
+
+===================
+Print Table Module
+===================
+
+The primary purpose of this package is to print out Gurobi variables in the console
+as tables.
+
+The PrintTable class takes data as a 2-tuple key, (row, col).
+
+There are optional arguments for headers and labels. There is a dimensions arg, but
+I might automate this in the future.
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='PrintTable',
+    url='https://github.com/MrRobboWilliamson/print-table',
+    author='Rob Williamson',
+    author_email='robmwilliamson@gmail.com',
+    # Needed to actually package something
+    packages=['print-table'],
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='This package provides a class to print tables nicely in the console',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.txt').read(),
+)
